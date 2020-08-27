911cde87688291e9929ddebc14af6e5a674fae30
Hoang Do <shinkenger.vn9x@gmail.com>
2020-08-27 10:24:34 +0700

    - Apply utils - Security modules.

--- auth/src/main/kotlin/vn/amit/auth/repository/AccountRepository.kt
+++ auth/src/main/kotlin/vn/amit/auth/repository/AccountRepository.kt
@@ -2,7 +2,7 @@ package vn.amit.auth.repository
 
 import org.springframework.stereotype.Repository
 import vn.amit.common.repository.BaseRepository
-import vn.amit.entity.auth.Account
+import vn.amit.entity.auth.User
 
 @Repository
-interface AccountRepository : BaseRepository<Account, Int>
+interface AccountRepository : BaseRepository<User, Int>
